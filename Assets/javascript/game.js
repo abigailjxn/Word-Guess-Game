8662464b4abf1b298f60e5026e628cf36e904225
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2018-09-13 23:45:39 -0500

    added more game logic

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -4,26 +4,44 @@
 
 // 1. Define Variables
 
-// Create list of movies
-// Randomly Choose word to serve as game 'movie word'
-// List letters in game 'movie word'
+// Create array of Movies
+// Randomly Choose word to serve as game 'movieWord'
+// List letters in game 'movieWord'
+// Create variable for letter key (?)
+// Create guessCounter to be [movieword.length + 5]
  
 
 // 2. Mechanics
 
+// GUESSING
 // Receive input of player letter key
-// Compare player letter key to list letters
-// If letter key == list letter and not out of strikes 
-    // record letter letter to 'correct'
+// Compare player letter key to movieWord list letter
+
+// If (letter key == any list letter && not already guessed)
+    // add letter key to 'correct' array
+    // display letter in UI
     // continue to receive letters
     //if letter is in 'correct' or 'guessed' already, do nothing
 
-// Else If letter key !== list letter and not out of strikes
-    // record letter to 'guessed'
-    // continue to receive letters
-    // if letter is in 'correct' or 'guessed' already, do nothing
+// Else If (letter key !== any list letter && not already guessed)
+    // add letter key to 'guessed'array
+    // display letter in UI
+    // guessCounter-- (minus 1)
+    // display counter in UI
+    // If letter key is in 'correct' or 'guessed' already, do nothing
 
-// Else (i.e. out of strikes) Game over
+// Else strikeCounter == 0
+    // go to loss
+
+// WINS / LOSSES
+// If correct array == movieWord array 
+    // win counter +1
+    // play song? or show clip? or some image?
+    // reset with new word, new strikeCounter
+
+// If strikeCoutner == 0
+    // Game over display
+    // reset with new word, new strikeCounter
 
 // 3. Display
 
