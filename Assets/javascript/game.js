ddfa415a90369a432a06f4ffcd6559801fa1b914
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2018-09-14 21:51:55 -0500

    properly connected reset.css and game.js files

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -6,11 +6,14 @@
 
 // Create array of Movies
     var Movies = ["Castle In the Sky", "My Neighbor Totoro", "Kiki's Delivery Service", "Porco Rosso", "Princess Mononoke", "Spirited Away", "Howl's Moving Castle", "Ponyo", "The Wind Rises"];
+    console.log(Movies);
 // Randomly Choose word to serve as game 'movieTitle'
     // Create variable that generates random number between 0 and Movies - 1
         var a = Math.floor(Math.random() * 9);
+        console.log(a);
     // Create variable for chosen word
     var movieTitle = Movies[a]
+    console.log(movieTitle);
 // List letters in game 'movieTitle'
 // Create variable for pressed key (?)
 // Create guessCounter to be [movieTitle.length + 5]
